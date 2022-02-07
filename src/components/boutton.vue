044385e3374d250219a8fb85b299a5a511bd8e8e
BenZitoune <pierre.delanou@ynov.com>
2022-02-07 15:11:55 +0100

    test

---
+++ src/components/boutton.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+defineProps ({
+    text: {
+        type: String,
+        default: 'Envoyer',
+        boutton1: Boolean,
+        
+    }
+})
+</script>
+
+<template>
+<div>
+    <v-for></v-for>
+        <button>
+            10
+        </button><br>
+    
+        <button>
+            1000
+        </button>
+
+</div>
+
+</template>
+
+
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,22 @@
-<script setup></script>
+<script setup>
+import bouttonVue from '../components/boutton.vue';
+
+
+const ex = "waf";
+let eyy = ["truc", "crabe"];
+</script>
+
+
 
 <template>
-    <div>
+    
         <h1>
-            About page
+            About page {{ex}} {{eyy}}
         </h1>
-    </div>
+        
+    <bouttonVue /> 
+    
+            <boutton1 />
+        
+         
 </template>
