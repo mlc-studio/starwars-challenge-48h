6bcee08703a10a3e7eae3427c50675bf3f18a61e
macsimtv <maximelecouturier@gmail.com>
2022-02-07 14:52:25 +0100

    [Add] Ajout d'un template pour la page d'accueil

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,31 @@
 </script>
 
 <template>
-    <div>
-        <h1>Home Page</h1>
-    </div>
+    <main class="home">
+        <section class="home__banner">
+            <div class="home__logo">
+
+            </div>
+            <div class="home__background">
+
+            </div>
+        </section>
+        <section class="home__quiz">
+            <div class="home__quiz__content">
+
+            </div>
+            <div class="home__quiz__image">
+                <img src="" alt="">
+            </div>
+        </section>
+        <section class="home__wiki">
+            <div class="home__wiki__image">
+
+            </div>
+            <div class="home__wiki__content">
+
+            </div>
+        </section>
+    </main>
+    
 </template>
