fbffe33383c4c3359fe393ef6e8ec5dedd293a96
lancelot260 <72740515+lancelot260@users.noreply.github.com>
2022-02-07 19:01:37 +0100

    header et debut page quiz

--- src/utils/router.js
+++ src/utils/router.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '../views/Home.vue';
 import About from '../views/About.vue';
+import Quiz from '../views/Quiz.vue';
 
 const routes = [
     {
@@ -13,6 +14,11 @@ const routes = [
         path: '/about',
         name: 'About',
         component: About,
+    },
+    {
+        path: '/quiz',
+        name: 'Quiz',
+        component: Quiz,
     }
 ];
 
---
+++ src/views/Quiz.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+
+<template>
+<div id="banner">
+    <a id="Firstnav">home</a>
+    <a id="Secondnav">quiz</a>
+    <a id="Thirdnav">wiki</a>
+</div>
+
+    <div class="contend">
+        <h1>
+            “Remember…the Force will be with you, always.”
+        </h1>
+        <button>
+            START
+        </button>
+    </div>
+
+    <a id="footer">Revenir à l'aceuil</a>
+    
+</template>
