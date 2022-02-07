c6aae0d24339659aa88e056a135d7e3cb7ea82d9
roulios44 <jules.croizier@gmail.com>
2022-02-07 15:12:16 +0100

    Mise a jour

---
+++ src/components/Bouton.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue';
+
+defineProps({
+    text: String,
+})
+
+const compteur = ref(0);
+
+
+const AddCompteur = () => {
+    compteur.value++
+    console.log(compteur.value)
+}
+
+</script>
+<template>
+<p>
+    {{ compteur }}
+</p>
+<button @click="AddCompteur" value={{bouton1}}>
+    {{text = "You can assing the text from the componement"}}
+</button>
+<br>
+<button value={{bouton2}}>
+    {{text = "Another one "}}
+</button>
+<form>
+    
+</form>
+<br>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,22 @@
 <script setup>
+import { ref } from 'vue';
+
+import Bouton from "../components/Bouton.vue"
+
+const exemple = ref('texy');
+let listeTest = ['mon', 'pre'];
+
+console.log(exemple.value)
 
 </script>
 
 <template>
     <div>
-        <h1>Home Page</h1>
+        <h1>Home</h1>
+        <p>{{ exemple }}</p>
+        <div>
+            <p v-for="mot, index in listeTest">{{ mot }}:{{ index }}</p>
+        </div>
+        <Bouton :text="exemple" />
     </div>
 </template>
