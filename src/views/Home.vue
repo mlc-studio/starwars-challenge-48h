ce82c424ac5d5c4160234ea2781effd2f7b28f96
roulios44 <jules.croizier@gmail.com>
2022-02-08 01:25:59 +0100

    Home html and css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,30 +12,29 @@ console.log(exemple.value)
 
 <template>
     <main class="home">
+        
         <section class="home__banner">
             <div class="home__logo">
-
             </div>
             <div class="home__background">
-
+                <img src="/img/fond-ecran_home.png" alt="Backgound Image">
             </div>
         </section>
         <section class="home__quiz">
             <div class="home__quiz__content">
-
+                <p>Texte Descriptif</p>
             </div>
             <div class="home__quiz__image">
-                <img src="" alt="">
+                <img src="/img/dark_vador.png" alt="Image for the Quiz">
             </div>
         </section>
         <section class="home__wiki">
             <div class="home__wiki__image">
-
+                <img src="/img/trooper.png" alt="Image à propos">
             </div>
             <div class="home__wiki__content">
-
+                <p>Content for wiki</p>
             </div>
         </section>
     </main>
-    
 </template>
