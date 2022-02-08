26e2e90912c322111648bd3c0a133b0a3050e96b
macsimtv <maximelecouturier@gmail.com>
2022-02-08 16:21:10 +0100

    [Test] Push

--- public/github-sync.php
+++ public/github-sync.php
@@ -5,7 +5,7 @@
 $folder = __DIR__ . DIRECTORY_SEPARATOR . 'tmp' . DIRECTORY_SEPARATOR;
 
 // Go to miaryrabs; pull and remove files inside tmp
-$cmd = 'cd /var/www/starwars-challenge-48h/; git pull origin master; rm -rf ' . $folder . '*';
+$cmd = 'cd /var/www/starwars-challenge-48h/; sudo git pull origin master; sudo npm i; sudo npm run build';
 
 // if calling from php cli
 if (isset($argv)) {
