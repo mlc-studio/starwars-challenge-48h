03b250fd32852cd46185481a71e0de4a15569402
lancelot260 <72740515+lancelot260@users.noreply.github.com>
2022-02-08 08:48:13 +0100

    image fond

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -6,7 +6,10 @@
     <a id="Secondnav">quiz</a>
     <a id="Thirdnav">wiki</a>
 </div>
-
+    <div id="imageBackgroud">
+        <img src="src\assets\picture\starsFond.png" alt="">
+    </div>
+    
     <div class="progress">
         <div id="check"></div>
         <div id="check"></div>
@@ -20,9 +23,7 @@
         <div id="check"></div>
     </div>
 
-    <div id="imageBackgroud">
-        <img src="src\assets\picture\starsFond.png" alt="">
-    </div>
+    
 
     <div class="separator" id="top">
     </div>
