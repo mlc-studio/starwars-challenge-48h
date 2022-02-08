e1d2226d5a3c00fdc0aaa146eafb764a6b0a19bd
macsimtv <maximelecouturier@gmail.com>
2022-02-08 20:21:29 +0100

    [Add] Ajout du quiz

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -1,18 +1,132 @@
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 
 import Header from "../components/Header.vue";
 
 const gameController = ref({
   playerScore: 0,
-  playerAnswer: [],
+  playerAnswers: [],
+  playerCurrentAnswer: undefined,
+
   isGameStart: false,
+  isGameFinish: false,
+  isValidate: false,
   step: 0,
-  questionCount: 10,
+
+  badPopup: "",
+  rightPopup: "",
+
+  questions: [
+    {
+      title: "Titre de la question ?",
+      answers: [
+        "this one is wrong",
+        "this one is wrong to",
+        "this one is right",
+        "Wrong bro!",
+      ],
+      rightAnswer: 2,
+    },
+    {
+      title: "Titre de la question 2 ?",
+      answers: [
+        "this one is wrong",
+        "this one is wrong to",
+        "this one is right",
+        "Wrong bro!",
+      ],
+      rightAnswer: 2,
+    },
+    {
+      title: "Titre de la question 3 ?",
+      answers: [
+        "this one is wrong",
+        "this one is wrong to",
+        "this one is right",
+        "Wrong bro!",
+      ],
+      rightAnswer: 2,
+    },
+    {
+      title: "Titre de la question 4 ?",
+      answers: [
+        "this one is wrong",
+        "this one is wrong to",
+        "this one is right",
+        "Wrong bro!",
+      ],
+      rightAnswer: 2,
+    },
+  ],
+  questionCount: 4,
 });
+
+onMounted(() => {
+  generateQuestions();
+})
+
+const generateQuestions = () => {
+
+}
 
 const onGameStart = () => {
   gameController.value.isGameStart = true;
+};
+
+const onChooseAnswer = (index) => {
+  if (gameController.value.isValidate) return;
+  gameController.value.playerCurrentAnswer = index;
+};
+
+const onValidate = () => {
+  if (gameController.value.playerCurrentAnswer == undefined) return;
+
+  gameController.value.isValidate = true;
+
+  if (
+    gameController.value.playerCurrentAnswer !=
+    gameController.value.questions[gameController.value.step].rightAnswer
+  ) {
+    gameController.value.playerAnswers.push(0);
+
+    gameController.value.rightPopup = "";
+    gameController.value.badPopup = `Bad answer, the correct one was: ${
+      gameController.value.questions[gameController.value.step].answers[
+        gameController.value.questions[gameController.value.step].rightAnswer
+      ]
+    }`;
+  }
+
+  if (
+    gameController.value.playerCurrentAnswer ==
+    gameController.value.questions[gameController.value.step].rightAnswer
+  ) {
+    gameController.value.playerAnswers.push(1);
+    gameController.value.playerScore++;
+
+    gameController.value.badPopup = "";
+    gameController.value.rightPopup = `You right ! The correct answer was: ${
+      gameController.value.questions[gameController.value.step].answers[
+        gameController.value.questions[gameController.value.step].rightAnswer
+      ]
+    }`;
+  }
+};
+
+const onNextQuestion = () => {
+  // Reset
+  gameController.value.isValidate = false;
+  gameController.value.rightPopup = "";
+  gameController.value.badPopup = "";
+  gameController.value.playerCurrentAnswer = undefined;
+
+  // Check if Finish
+  if (gameController.value.step + 1 == gameController.value.questionCount) {
+    gameController.value.isGameFinish = true;
+  }
+
+  // Next
+  gameController.value.step++;
 };
 </script>
 
@@ -26,7 +140,7 @@ const onGameStart = () => {
         :key="count"
       >
         <svg
-          v-if="gameController.playerAnswer[count - 1] === 1"
+          v-if="gameController.playerAnswers[count - 1] === 1"
           class="quiz__icon"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
@@ -39,7 +153,7 @@ const onGameStart = () => {
           />
         </svg>
         <svg
-          v-if="gameController.playerAnswer[count - 1] === 0"
+          v-if="gameController.playerAnswers[count - 1] === 0"
           class="quiz__icon"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
@@ -53,47 +167,86 @@ const onGameStart = () => {
         </svg>
       </div>
     </div>
-    <div class="quiz__content container" :class="{ 'quiz__content--centered': !gameController.isGameStart }">
+    <div class="quiz__heading container" v-if="gameController.isGameFinish">
+      <h2 class="heading--h2 heading--stroke uppercase">Score</h2>
+    </div>
+    <div
+      class="quiz__content container"
+      :class="{ 'quiz__content--centered': !gameController.isGameStart || gameController.isGameFinish }"
+    >
       <template v-if="!gameController.isGameStart">
         <h4 class="quiz__title heading--h1 heading--stroke heading--white">
           “Remember…the Force will be with you, always.”
         </h4>
         <button @click.prevent="onGameStart" class="btn">Start Now</button>
       </template>
-      <template v-if="gameController.isGameStart">
-          <div class="quiz__game">
-              <div class="quiz__game-head">
-
+      <template
+        v-if="gameController.isGameStart && !gameController.isGameFinish"
+      >
+        <div class="quiz__game">
+          <div class="quiz__game-head">
             <h3 class="heading--h1 heading--stroke">
-                Question number: {{ gameController.step + 1 }}
+              Question number: {{ gameController.step + 1 }}
             </h3>
             <h2 class="quiz__game-question heading--h3">
-                Intitulé de la question
+              {{ gameController.questions[gameController.step].title }}
             </h2>
-              </div>
-            <div class="quiz__game-separator"></div>
-            <div class="quiz__game-answers">
-                <div class="quiz__game-answer">
-                    <label for="answer-1">Intitulé de la answer</label>
-                    <input type="radio" id="answer-1" name="game-answer" />
-                </div>
-                <div class="quiz__game-answer">
-                    <label for="answer-2">Intitulé de la answer</label>
-                    <input type="radio" id="answer-2" name="game-answer" />
-                </div>
-                <div class="quiz__game-answer">
-                    <label for="answer-3">Intitulé de la answer</label>
-                    <input type="radio" id="answer-3" name="game-answer" />
-                </div>
-                <div class="quiz__game-answer">
-                    <label for="answer-4">Intitulé de la answer</label>
-                    <input type="radio" id="answer-4" name="game-answer" />
-                </div>
-            </div>
-            <button class="btn">
-                Next Question
-            </button>
           </div>
+          <div class="quiz__game-separator"></div>
+          <div class="quiz__game-answers">
+            <div
+              @click.prevent="onChooseAnswer(index)"
+              v-for="(answer, index) in gameController.questions[
+                gameController.step
+              ].answers"
+              :key="index"
+              class="quiz__game-answer"
+              :class="{ active: gameController.playerCurrentAnswer == index }"
+            >
+              <label :for="`answer-${index}`">{{ answer }}</label>
+              <input type="radio" :id="`answer-${index}`" name="game-answer" />
+            </div>
+          </div>
+          <div v-if="gameController.badPopup || gameController.rightPopup">
+            <p>
+              {{ gameController.badPopup || gameController.rightPopup }}
+            </p>
+          </div>
+          <button
+            v-if="!gameController.isValidate"
+            @click.prevent="onValidate"
+            class="btn"
+          >
+            Validate
+          </button>
+          <button
+            v-if="gameController.isValidate"
+            @click.prevent="onNextQuestion"
+            class="btn"
+          >
+            <template
+              v-if="
+                gameController.step + 1 ==
+                gameController.questionCount
+              "
+            >
+              Show Results
+            </template>
+            <template v-else> Next Question </template>
+          </button>
+        </div>
+      </template>
+      <template v-if="gameController.isGameFinish">
+        <div class="quiz__score-container">
+          <p class="quiz__score-citation">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita voluptates omnis, aliquam distinctio odit excepturi labore nobis voluptate praesentium rerum laborum sapiente dolorum iusto fugit debitis alias ipsam incidunt sed?
+          </p>
+          <h2 class="quiz__score-title heading--stroke">
+          {{ gameController.playerScore }} / {{ gameController.questionCount }}
+          </h2>
+          <router-link class="btn btn--outline" to="/">Go back Home</router-link>
+        </div>
+        
       </template>
     </div>
     <div class="quiz__footer container">
