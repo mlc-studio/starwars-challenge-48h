7b1c0270efb54206fce16bd50d8ca06c4ca4a2de
roulios44 <jules.croizier@gmail.com>
2022-02-08 08:13:17 +0100

    Home page + banner

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,10 +11,17 @@ console.log(exemple.value)
 </script>
 
 <template>
-    <main class="home">
-        
+    <main class="home">    
         <section class="home__banner">
+            <div class="home__banner__top">
+                <ul>
+                    <li><router-link to="/home"><p>Home</p></router-link></li>
+                    <li><router-link to="/quiz"><p>Quiz</p></router-link></li>
+                    <li><router-link to="/wiki"><p>Wiki</p></router-link></li>
+                </ul>
+            </div>
             <div class="home__logo">
+                <img scr="/img/starwars_logo.png" alt="Logo Star Wars">
             </div>
             <div class="home__background">
                 <img src="/img/fond-ecran_home.png" alt="Backgound Image">
@@ -30,7 +37,7 @@ console.log(exemple.value)
         </section>
         <section class="home__wiki">
             <div class="home__wiki__image">
-                <img src="/img/trooper.png" alt="Image à propos">
+                <img src="/img/trooper.png" alt="Image Wiki">
             </div>
             <div class="home__wiki__content">
                 <p>Content for wiki</p>
