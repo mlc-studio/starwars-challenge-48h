9893e9cad15d502c9809284d3baa924453356daa
macsimtv <maximelecouturier@gmail.com>
2022-11-10 11:13:54 +0100

    [Fix]

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
-<script setup></script>
 
 <template>
+  <ReloadPWA />
   <router-view />
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import ReloadPWA from "./components/ReloadPWA.vue";
+export default defineComponent({
+  name: "App",
+  components: {
+    ReloadPWA,
+  },
+});
+</script>
 
 <style lang="scss">
 @import './assets/scss/main.scss';
